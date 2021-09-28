ed22d10b1f54bafaaa6030c6ac92b7bbecdfdcab
Jacó Apolinário <jacoapollinario@gmail.com>
2021-09-28 09:05:37 -0300

    Fix

--- src/modules/cars/infra/typeorm/repositories/CarsRepository.ts
+++ src/modules/cars/infra/typeorm/repositories/CarsRepository.ts
@@ -1,8 +1,9 @@
 import { getRepository, Repository } from 'typeorm';
 
-import { ICreateCarDTO } from '../dtos/ICreateCarDTO';
-import { Car } from '../infra/typeorm/entities/Car';
-import { ICarsRepository } from './ICarsRepository';
+import { ICreateCarDTO } from '@modules/cars/dtos/ICreateCarDTO';
+import { ICarsRepository } from '@modules/cars/repositories/ICarsRepository';
+
+import { Car } from '../entities/Car';
 
 class CarsRepository implements ICarsRepository {
   private repository: Repository<Car>;
