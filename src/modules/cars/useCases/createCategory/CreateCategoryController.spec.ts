324b1ad2bbed44eb4433a85f417991fb963053b3
Jacó Apolinário <jacoapollinario@gmail.com>
2021-12-13 19:07:25 -0300

    feat(rentals): news features supertest

--- src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
@@ -1,9 +1,15 @@
+import { response } from 'express';
 import request from 'supertest';
 
 import { app } from '@shared/infra/http/app';
 
 describe('Create Category Controller', () => {
-  it('test', async () => {
-    await request(app).get('/cars/available').expect(200);
+  it('Should be able to create a new category', async () => {
+    await request(app).get('/categories').send({
+      name: 'Category Supertest',
+      description: 'Category Supertest',
+    });
+
+    expect(response.status).toBe(201);
   });
 });
