1e8213d5db024f0c7dce1b22583b495d718b0c3c
Jacó Apolinário <jacoapollinario@gmail.com>
2022-05-30 10:05:14 -0300

    add controller refreshToken

--- src/shared/infra/http/middlewares/ensureAuthenticated.ts
+++ src/shared/infra/http/middlewares/ensureAuthenticated.ts
@@ -1,7 +1,8 @@
 import { NextFunction, Request, Response } from 'express';
 import { verify } from 'jsonwebtoken';
 
-import { UsersRepository } from '@modules/accounts/infra/typeorm/repositories/UsersRepository';
+import auth from '@config/auth';
+import { UsersTokensRepository } from '@modules/accounts/infra/typeorm/repositories/UsersTokensRepository';
 import { AppError } from '@shared/errors/AppError';
 
 interface IPayload {
@@ -15,20 +16,24 @@ export async function ensureAuthenticated(
 ) {
   const authHeader = request.headers.authorization;
 
+  const usersTokensRepository = new UsersTokensRepository();
+
   if (!authHeader) {
     throw new AppError('Token missing', 401);
   }
 
   const [, token] = authHeader.split(' ');
+
   try {
     const { sub: user_id } = verify(
       token,
-      'cfe275a5908b5650488e0b0342c2d6cc',
+      auth.secret_refresh_token,
     ) as IPayload;
 
-    const usersRepository = new UsersRepository();
-
-    const user = usersRepository.findById(user_id);
+    const user = usersTokensRepository.findByUserIdAndRefreshToken(
+      user_id,
+      token,
+    );
 
     if (!user) {
       throw new AppError('User does not exists', 401);
